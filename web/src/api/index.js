c5159666b30f7ce0ec73ceebcef0c510ba057a02
xiaodashi12 <43960032+xiaodashi12@users.noreply.github.com>
2021-11-14 20:43:29 +0800

    admin前端用户管理对接 (#9)
    * first
    
    * 代码调整删除
    
    * license加到文件开头
    
    * license加到文件开头
    
    Co-authored-by: 陈建国 <chenjg26809@hundsun.com>

--- web/src/api/index.js
+++ web/src/api/index.js
@@ -14,12 +14,78 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 const config = {
     'base': {
         url: '/api/base',
         method: ''
     },
-    
+    'whistle':{
+        url: '/users/whistle',
+        method: ''
+    },
+    'resetPwd':{
+        url: '/operator/resetPassword',
+        method: ''
+    },
+    'findHisList':{
+        url: '/electronicAccountHis/findHisList',
+        method: ''
+    },
+    'cancleSign':{
+        url: '/electronicAccountHis/cancleSign',
+        method: ''
+    },
+    'allChannel':{
+        url: '/findAllChannel',
+        method: ''
+    },
+    'closeChannel':{
+        url: '/closeChannel',
+        method: ''
+    },
+    'openChannel':{
+        url: '/openChannel',
+        method: ''
+    },
+    'findCustomer':{
+        url: '/redis/findCustomerCar',
+        method: ''
+    },
+    'findRedis':{
+        url: '/redis/findRedisValue',
+        method: ''
+    },
+    'removeCustomer':{
+        url: '/redis/removeCustomerCar',
+        method: ''
+    },
+    'findCardPub':{
+        url: '/cardpub/findCardPub',
+        method: ''
+    },
+    'cardLost':{
+        url: '/cardpub/cardLost',
+        method: ''
+    },
+    'cardUnLost':{
+        url: '/cardpub/cardUnLost',
+        method: ''
+    },
+    'register':{
+        url: '/register',
+        method: ''
+    },
+    'login':{
+        url: '/login',
+        method: ''
+    },
+    'getInfo':{
+        url: '/user/getInfo',
+        method: ''
+    },
+    'getPasswordEdit':{
+        url: '/user/password/edit',
+        method: ''
+    },
 }
 export default config;
